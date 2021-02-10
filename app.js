5dc627ccb3ce1e727a8cf3f2a0e3b81f8beca241
Kalan Prudhomme <cleaver9@gmail.com>
2021-02-10 14:36:34 -0800

    interactive tag

---
+++ app.js
@@ -0,0 +1,10 @@
+const nameInput = document.getElementById('name-input');
+const changeButton = document.getElementById('name-change-button');
+const nameDisplay = document.getElementById('name');
+
+changeButton.addEventListener('click', () => {
+    nameDisplay.textContent =  nameInput.value;
+    nameInput.value='';
+});
+
+
